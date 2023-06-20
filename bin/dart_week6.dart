e440138bc4ba00fb722cadd1d591613f83abc3af
phisit77 <kachidoki787@gmail.com>
2023-06-20 13:43:12 +0700

    first intential

---
+++ bin/dart_week6.dart
@@ -0,0 +1,12 @@
+
+
+void main (){
+  int x =10;
+  double y = 5.5;
+  print("Arithemetic x + y: $x + $y = ${x + y}");//1.Addition
+  print("Arithemetic x - y: ${x - y}");//2.Subtraction
+  print("Arithemetic x * y: ${x * y}");//3.multiply
+  print("Arithemetic x / y: ${x / y}");//4.Division
+  print("Arithemetic x ~/ y: ${x ~/ y}");//5. Divisionm returning integer
+  print("Arithemetic x % y: ${x % y}");//6. Modulus
+}
